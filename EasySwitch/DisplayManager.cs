2e569fc4c02922eb608108db8bc0377bf3d2ef32
Tom Willemse <tom@ryuslash.org>
2014-06-01 23:15:08 +0200

    Initial commit

---
+++ EasySwitch/DisplayManager.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EasySwitch
+{
+    class DisplayManager
+    {
+        private DEVMODE mode;
+
+        const int ENUM_CURRENT_SETTINGS = -1;
+
+        const int DISP_CHANGE_SUCCESSFUL = 0;
+        const int DISP_CHANGE_BADMODE = -2;
+        const int DISP_CHANGE_FAILED = -1;
+        const int DISP_CHANGE_RESTART = 1;
+
+        #region External functions
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern Boolean EnumDisplaySettings(
+            [param: MarshalAs(UnmanagedType.LPTStr)]
+            string lpszDeviceName,
+            [param: MarshalAs(UnmanagedType.U4)]
+            int iModeNum,
+            [In, Out]
+            ref DEVMODE lpDevMode);
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern int ChangeDisplaySettings(
+            [In, Out]
+            ref DEVMODE lpDevMode,
+            [param: MarshalAs(UnmanagedType.U4)]
+            uint dwflags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct DEVMODE
+        {
+            // You can define the following constant
+            // but OUTSIDE the structure because you know
+            // that size and layout of the structure
+            // is very important
+            // CCHDEVICENAME = 32 = 0x50
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+            // In addition you can define the last character array
+            // as following:
+            //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            //public Char[] dmDeviceName;
+
+            // After the 32-bytes array
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmSpecVersion;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmDriverVersion;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmSize;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmDriverExtra;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmFields;
+
+            public POINTL dmPosition;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayOrientation;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFixedOutput;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmColor;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmDuplex;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmYResolution;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmTTOption;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmCollate;
+
+            // CCHDEVICENAME = 32 = 0x50
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+            // Also can be defined as
+            //[MarshalAs(UnmanagedType.ByValArray,
+            //    SizeConst = 32, ArraySubType = UnmanagedType.U1)]
+            //public Byte[] dmFormName;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmLogPixels;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmBitsPerPel;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPelsWidth;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPelsHeight;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFlags;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFrequency;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmICMMethod;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmICMIntent;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmMediaType;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDitherType;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmReserved1;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmReserved2;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPanningWidth;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPanningHeight;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINTL
+        {
+            [MarshalAs(UnmanagedType.I4)]
+            public int x;
+            [MarshalAs(UnmanagedType.I4)]
+            public int y;
+        }
+        #endregion
+
+        public DisplayManager()
+        {
+            mode = new DEVMODE();
+            mode.dmSize = (ushort)Marshal.SizeOf(mode);
+
+            EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref mode);
+        }
+
+        public uint GetBitsPerPixel()
+        {
+            return mode.dmBitsPerPel;
+        }
+
+        public void SetBitsPerPixel(uint bits)
+        {
+            mode.dmBitsPerPel = bits;
+
+            ChangeDisplaySettings(ref mode, 0);
+        }
+    }
+}
---
+++ EasySwitch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace EasySwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayManager dm = new DisplayManager();
+            uint bpp = dm.GetBitsPerPixel();
+
+            dm.SetBitsPerPixel(bpp == 16u ? 32u : 16u);
+        }
+    }
+}
